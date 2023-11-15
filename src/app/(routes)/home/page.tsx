03a089a80b291d06c136ab03331dd1948e3f5acd
Vuxuanan1998 <anvu1308@gmail.com>
2023-11-16 06:22:54 +0700

    init project

---
+++ src/app/(routes)/home/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+const admin = () => {
+  return (
+    <div>
+      <h1>hihi</h1>
+    </div>
+  );
+};
+
+export default admin;
---
+++ src/app/(routes)/smartCart/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+const admin = () => {
+  return (
+    <div>
+      <h1>smartCart</h1>
+    </div>
+  );
+};
+
+export default admin;
